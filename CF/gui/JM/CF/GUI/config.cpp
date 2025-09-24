1e14394a970fcb8bcf34cee520728d738f83a92d
MaxFlint222 <maxfljnt220@gmail.com>
2025-09-24 05:11:30 +0300

    Add files via upload

---
+++ CF/gui/JM/CF/GUI/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.98
+//https://mikero.bytex.digital/Downloads
+//'now' is Wed Sep 24 04:52:21 2025 : 'file' last modified on Sat Jan 18 23:29:26 2025
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class JM_CF_GUI
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
