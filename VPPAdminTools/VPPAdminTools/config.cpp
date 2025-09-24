3f3bce2b0e84ad176513434da94b9d1a89687459
MaxFlint222 <maxfljnt220@gmail.com>
2025-09-24 07:29:23 +0300

    Add files via upload

---
+++ VPPAdminTools/VPPAdminTools/config.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: config.bin
+//Produced from mikero's Dos Tools Dll version 9.98
+//https://mikero.bytex.digital/Downloads
+//'now' is Wed Sep 24 04:17:27 2025 : 'file' last modified on Wed Sep 24 04:17:27 2025
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+class CfgPatches
+{
+	class DZM_VPPAdminToolsScripts
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
